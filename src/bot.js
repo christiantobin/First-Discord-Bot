679583d7123cdf1de8d2632450a46d68abc6dfe8
christian@wirelesseco.com <christian@wirelesseco.com>
2021-06-30 17:55:40 -0700

    Cleaning

--- src/bot.js
+++ src/bot.js
@@ -14,8 +14,14 @@ client.login(process.env.DISCORDJS_BOT_TOKEN);
 
 client.on("ready", function() {
 	console.log("READY FOR ACTION!");
-    client.user.setActivity('yes/no questions followed by \'??\'.', {type: 'LISTENING'});
+    client.user.setActivity('!!help.', {type: 'LISTENING'});
 });
+
+client.on('message', msg => {
+    if (msg.content.toString() == '!!help'){
+        msg.reply("\n**Commands:**\n!!rayne\n!!roulette\nOr type a question followed by \'??\'")
+    }
+})
 
 client.on('message', msg => {
     if (msg.content.length == 69)
@@ -23,7 +29,7 @@ client.on('message', msg => {
 });
 
 client.on('message', msg => {
-    if (msg.content.includes('??'))
+    if (msg.content.includes('??') && !msg.author.bot)
     {
         const s = getRandomInt(7);
         switch(s){
@@ -44,7 +50,7 @@ client.on('message', msg => {
 });
 
 client.on('message', async msg => {
-    if (msg.content.toString() == '!rayne')
+    if (msg.content.toString() == '!!rayne')
     {
         const res = await fetch('https://www.mentalfloss.com/api/facts?page=2&limit=1&cb=0.3276683361034485').then(response => response.json());
         const message = "**Random fact:** " + res[0].fact; 
@@ -53,7 +59,7 @@ client.on('message', async msg => {
 })
 
 client.on('message', async msg => {
-    if (msg.content.toString() == '!roulette')
+    if (msg.content.toString() == '!!roulette')
     {
         switch (getRandomInt(5)){
             case 0:
@@ -62,7 +68,6 @@ client.on('message', async msg => {
             default:
                 msg.reply('💦🔫 Whew. You\'re safe.');
                 break;
-
         }
     }
 })
