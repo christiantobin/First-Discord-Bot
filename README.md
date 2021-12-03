13c302cc8658382611b31dbcd63d02c0ca56e763
christiantobin <christian@wirelesseco.com>
2021-12-02 16:52:56 -0800

    Some additions

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
 # Tobin's Discord Bot
-Add the bot to your server by clicking [here]()
-## Commands
-### - !!rayne
-- Messages a random fact
-### - !!roulette
-- Plays russian roulette
-### - !!balance
-- Checks your balance
-### - Magic 8 ball
-- Instr.: Type a yes/no question followed by '??'
 
+Add the bot to your server by clicking [here]()
+
+## Commands
+
+### - !!rayne
+
+-   Messages a random fact
+
+### - !!roulette
+
+-   Plays russian roulette
+
+### - !!balance
+
+-   Checks your balance
+
+### - Magic 8 ball
+
+-   Instr.: Type a yes/no question followed by '??'
 
 ## Future Features/Improvements
-### - !!blackjack 
-- will let players play blackjack and sqlite will keep track of their earnings/losses (fake money)
+
+### - !!blackjack
+
+-   will let players play blackjack and sqlite will keep track of their earnings/losses (fake money)
+
 ### - !!stocks
-- will let players 'invest' in stocks and sqlite will keep track of their earnings/losses
+
+-   will let players 'invest' in stocks and sqlite will keep track of their earnings/losses
--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,7 @@
-
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+const dotenv = require("dotenv").config();
 const eco = require("discord-economy");
-const dotenv = require('dotenv').config();
 const Discord = require("discord.js");
-const { callbackify } = require('util');
 const client = new Discord.Client();
 
 function getRandomInt(max) {
@@ -14,18 +12,17 @@ client.login(process.env.DISCORDJS_BOT_TOKEN);
 
 client.on("ready", function () {
     console.log("READY FOR ACTION!");
-    client.user.setActivity('!!help.', { type: 'LISTENING' });
+    client.user.setActivity("!!help.", { type: "LISTENING" });
 });
 
-client.on('message', async msg => {
-
+client.on("message", async (msg) => {
     if (msg.author.bot) return;
     /**
      * Help command
      *  Tells users what commands there are
-     *  __ is underline ** is bold 
+     *  __ is underline ** is bold
      */
-    if (msg.content.toString() == '!!help') {
+    if (msg.content.toString() == "!!help") {
         msg.reply(`**Commands:**
         __!!rayne__ - tells a random fact
         __!!roulette__ - plays a simple game
@@ -33,62 +30,70 @@ client.on('message', async msg => {
         __!!coinflip <heads | tails> <bet amount>__ - play 50/50  
         
         Or type a question followed by '??' for a yes/no answer.
-        `)
+        `);
+        return;
     }
 
     /**
      * 69
      *  Fun message is sent if prior message was 69 chars long
      */
-    if (msg.content.length == 69)
+    if (msg.content.length == 69) {
         msg.reply("That message was a nice length ;)");
+        return;
+    }
 
     /**
      * ??
      *  Yes/no answers to questions
      *  Switch statement helps with proabilty. (More yes'!)
      */
-    if (msg.content.includes('??')) {
+    if (msg.content.includes("??")) {
         const s = getRandomInt(7);
         switch (s) {
             case 3:
-                msg.reply('Sure.');
+                msg.reply("Sure.");
                 break;
             case 2:
-                msg.reply('No.');
+                msg.reply("No.");
                 break;
             case 1:
-                msg.reply('Perhaps.');
+                msg.reply("Perhaps.");
                 break;
             default:
-                msg.reply('Yes.');
+                msg.reply("Yes.");
                 break;
-        };
+        }
+        return;
     }
 
     /**
      * Rayne
      *  Tells user a random face
      */
-    if (msg.content.toString() == '!!rayne') {
-        const res = await fetch('https://www.mentalfloss.com/api/facts?page=2&limit=1&cb=0.3276683361034485').then(response => response.json());
+    if (msg.content.toString() == "!!rayne") {
+        const res = await fetch(
+            "https://www.mentalfloss.com/api/facts?page=2&limit=1&cb=0.3276683361034485"
+        ).then((response) => response.json());
         const message = "**Random fact:** " + res[0].fact;
         msg.channel.send(message);
+        return;
     }
 
     /**
      * Roulette
-     *  Play russain roulette
+     *  Play russian roulette
      */
-    if (msg.content.toString() == '!!roulette') {
+    if (msg.content.toString() == "!!roulette") {
         switch (getRandomInt(5)) {
             case 0:
-                msg.reply('💥🔫 Sorry you lost.');
+                msg.reply("💥🔫 Sorry you lost.");
                 break;
             default:
-                msg.reply('💦🔫 Whew. You\'re safe.');
+                msg.reply("💦🔫 Whew. You're safe.");
                 break;
         }
+        return;
     }
 
     /*************************************
@@ -96,50 +101,75 @@ client.on('message', async msg => {
      *************************************/
 
     //Currency symbol: ₦
-    var args = msg.content.split(' ').slice(1);
+    var args = msg.content.split(" ").slice(1);
 
     /**
      * Admin command
      */
-    if (msg.content.includes('!!addNips')) {
+    if (msg.content.includes("!!addNips")) {
         if (msg.member.user.id == 295035247718825984)
-            eco.AddToBalance(args[0], args[1])
-        else
-            msg.reply("No can do buckaroo.");
+            eco.AddToBalance(args[0], args[1]);
+        else msg.reply("No can do buckaroo.");
+        return;
     }
 
     /**
      * Balance
      *  returns amount of nips someone has
      */
-    if (msg.content == '!!balance' || msg.content == '!!bal') {
-        var output = await eco.FetchBalance(msg.author.id)
+    if (msg.content == "!!balance" || msg.content == "!!bal") {
+        var output = await eco.FetchBalance(msg.author.id);
         msg.reply(`Your balance is ₦${output.balance}.00 nips.`);
+        return;
     }
 
     /**
      * Coinflip
+     * @author Eco
      */
-    if (msg.content.includes('!!coinflip') || msg.content.includes('!!cf')) {
+    if (msg.content.includes("!!coinflip") || msg.content.includes("!!cf")) {
+        var flip = args[0]; //Heads or Tails
+        var amount = args[1]; //Coins to gamble
 
-        var flip = args[0] //Heads or Tails
-        var amount = args[1] //Coins to gamble
+        if (!flip || !["heads", "tails"].includes(flip))
+            return msg.reply("Please specify the flip, either heads or tails!");
+        if (!amount) return msg.reply("Specify the amount you want to gamble!");
 
-        if (!flip || !['heads', 'tails'].includes(flip)) return msg.reply('Please specify the flip, either heads or tails!')
-        if (!amount) return msg.reply('Specify the amount you want to gamble!')
+        var output = await eco.FetchBalance(msg.author.id);
+        if (output.balance < amount)
+            return msg.reply(
+                "You have fewer ₦ than the amount you want to gamble!"
+            );
 
-        var output = await eco.FetchBalance(msg.author.id)
-        if (output.balance < amount) return msg.reply('You have fewer ₦ than the amount you want to gamble!')
-
-        var gamble = await eco.Coinflip(msg.author.id, flip, amount).catch(console.error);
-        msg.reply(`You ${gamble.output}! New balance: ₦${gamble.newbalance}.00`)
-
+        var gamble = await eco
+            .Coinflip(msg.author.id, flip, amount)
+            .catch(console.error);
+        msg.reply(
+            `You ${gamble.output}! New balance: ₦${gamble.newbalance}.00`
+        );
+        return;
     }
 
     /**
      * Blackjack
+     * @author Christian Tobin
      */
-    if (msg.content.includes('!!blackjack')) {
+    if (msg.content.includes("!!blackjack")) {
+    }
 
+    /**
+     * Stocks
+     * @author Christian Tobin
+     */
+    if (msg.content.includes("!!stocks")) {
+        //arg[0] = command, arg[1] = stock name, arg[2] = # of shares,
+        switch (args[0]) {
+            case "buy":
+                break;
+            case "sell":
+                break;
+            default:
+                break;
+        }
     }
 });
